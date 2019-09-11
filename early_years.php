1eadedeb9bcb1381e0db7e4fc1fa49570ab0e13b
jbt19981007 <53458427+jbt19981007@users.noreply.github.com>
2019-09-11 16:24:06 -0400

    Add files via upload

---
+++ early_years.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+
+	<meta charset="utf-8">
+
+	<title>Raphael Sanzio|Lab 1:Initial Personal Website</title>
+
+	<link rel="stylesheet" href="css/styles.css">
+
+</head>
+
+<body>
+
+	<div class="container">
+
+		<header>
+
+		<a href="index.php">
+
+			<h1>Raphael Sanzio</h1>
+
+			<img src="images/Raffaello_Sanzio.jpg">
+
+		</a>
+
+		
+
+	</header>
+
+	<aside>
+
+		<ul>
+
+			<li>Italian Painter and Architect</li>
+
+			<li>Painter of Madonnas</li>
+
+			<li>Artist of High Renaissance</li>
+
+		</ul>
+
+	</aside>
+
+	<main>
+
+		<h1>Early Years</h1>
+
+		<p>Raphael was the son of Giovanni Santi and Magia di Battista Ciarla; his mother died in 1491. His father was, according to the 16th-century artist and biographer Giorgio Vasari, a painter “of no great merit.” He was, however, a man of culture who was in constant contact with the advanced artistic ideas current at the court of Urbino. He gave his son his first instruction in painting, and, before his death in 1494, when Raphael was 11, he had introduced the boy to humanistic philosophy at the court.</p>
+
+	</main>
+
+	<footer>
+
+		<span>CSC 174: Advanced Front-end Web Design and Development</span>
+
+	</footer>
+
+	<?php include "inc/nav.php"; ?>
+
+	</div>
+
+	
+
+</body>
+
+<script src="http://code.jquery.com/jquery.js"></script>
+
+<script src="js/menu-highlighter.js"></script>
+
+</html>
