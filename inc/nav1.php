b6135cf05722cbcf1d64148fd902dde895f26f4a
jbt19981007 <53458427+jbt19981007@users.noreply.github.com>
2019-09-11 16:37:54 -0400

    Add files via upload

---
+++ inc/nav1.php
@@ -0,0 +1,17 @@
+<nav>
+
+		<ul>
+
+			<li><a href="index.php">Home</a></li>
+
+			<li><a href="early_years.php">Early Years</a></li>
+
+			<li><a href="perugia.php">Perugia</a></li>
+
+			<li><a href="rome.php">Rome</a></li>
+
+		</ul>
+
+	</nav>
+
+	
