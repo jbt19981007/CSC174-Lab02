828d575c18c4681acd781320eec876858405867a
rakomea <55210364+rakomea@users.noreply.github.com>
2019-09-11 16:40:53 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,27 @@
-#!/usr/bin/env wolframscript
+<?php $pageTitle="Home"; ?>
+<?php include "inc/html-top.php"; ?>
+<body class="container">
+<header >
+	<a href="index.php">
+		<h1>Richard  and Raphael</h1>
+        <img src="images/richie.jpg" alt="Home Photo">
+    </a>
+</header>
+<aside>
+	<ul>
+	<li>Essays about people</li>
+	<li>Mundane</li>
+	<li>Students of Computer Science</li>
+	</ul>
+</aside>
+<?php include "inc/homenav.php"; ?>
+<main>
+  Welcome to the page for the group Brooklyn99
+</main>
+
+<footer >CSC 174: Advanced Front-end Web Design and Development</footer>
+<?php include "inc/scripts.php";?>
+  
+</body>
+
+</html>
