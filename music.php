e243d33e175920a350b675859b9c9272e78cb151
rakomea <55210364+rakomea@users.noreply.github.com>
2019-09-11 16:06:34 -0400

    Add files via upload

---
+++ music.php
@@ -0,0 +1,28 @@
+<?php $pageTitle="Richard Akomea-Music"; ?>
+<?php include "inc/html-top.php"; ?>
+<body class="container">
+<header >
+	<a href="index.php">
+		<h1>Richard O Akomea</h1>
+        <img src="images/richie.jpg" alt="Richard Akomea Photo">
+    </a>
+</header>
+<aside>
+	<ul>
+	<li>Exceptionally Smooth</li>
+	<li>Magnificent and Ghetto</li>
+	<li>Student of Computer Science</li>
+	</ul>
+</aside>
+<?php include "inc/nav.php"; ?>
+<main>
+	<h1>Music</h1>
+	<p>Richard began his early production career in the mid-2010s, creating beats primarily for himself, an upcoming local artist, eventually developing a style that involved a combination of styles amany famous artists. His first official song came at the age of eighteen when he produced and rapped on  YOLO, a single he put out in 2015 with Tony Dickson. For a while now, Richard has not released any musical projects but has appeared on other albums such as Jamal Damiens recital released on soundcloud.  Richard also has a history of organising musical concerts with the 2015 Singspiration being evidence of that. In 2015 he alongside members of the Accra Academy Scripture Union Organised an Singspiration which saw a gathering of most of the prominent high schools in Accra at the time for a gospel music concert. </p>
+</main>
+
+<footer >CSC 174: Advanced Front-end Web Design and Development</footer>
+<?php include "inc/scripts.php";?>
+  
+</body>
+
+</html>
