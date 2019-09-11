59607772ae9a1a217e5def74aa56a39f7ae5de7d
rakomea <55210364+rakomea@users.noreply.github.com>
2019-09-11 16:22:49 -0400

    Add files via upload

---
+++ earlylife.php
@@ -0,0 +1,28 @@
+<?php $pageTitle="Richard Akomea-Early Life"; ?>
+<?php include "inc/html-top.php"; ?>
+<body class="container">
+<header >
+	<a href="index.php">
+		<h1>Richard O Akomea</h1>
+        <img src="images/richie.jpg" alt="Richard Akomea Photo">
+    </a>
+</header>
+<aside>
+	<ul>
+	<li>Exceptionally Smooth</li>
+	<li>Magnificent and Ghetto</li>
+	<li>Student of Computer Science</li>
+	</ul>
+</aside>
+<?php include "inc/nav.php"; ?>
+<main>
+	<h1>Early Life</h1>
+	<p>Richard's parents divorced when he was ten years old. After the divorce, he and his Father remained in Accra, while his mother returned to Batsonaa, where she pursued her Masters Degree in Education and went on to teach Twi in S.D.A Primary Schools. Joyce's limited finances and health issues caused her to remain close to Accra  until Richard's early adulthood. Three years after their divorce , Richard met and married Paulinaa Ohenewaa, a Trader. Richard was raised in two Ghanaian neighbourhoods. He lived on Okle Mantse Road in Accra  until grade nine, playing soccer with the Sakumonon School complex team. He then moved to Gbawe in 2012 to live with his Father and Step-mother after their marriage. </p>
+</main>
+
+<footer >CSC 174: Advanced Front-end Web Design and Development</footer>
+<?php include "inc/scripts.php";?>
+  
+</body>
+
+</html>
---
+++ richard.php
@@ -0,0 +1,27 @@
+<?php $pageTitle="Lab 1: Initial Personal Website"; ?>
+<?php include "inc/html-top.php"; ?>
+<body class="container">
+<header >
+	<a href="index.php">
+		<h1>Richard O Akomea</h1>
+        <img src="images/richie.jpg" alt="Richard Akomea Photo">
+    </a>
+</header>
+<aside>
+	<ul>
+	<li>Exceptionally Smooth</li>
+	<li>Magnificent and Ghetto</li>
+	<li>Student of Computer Science</li>
+	</ul>
+</aside>
+<?php include "inc/nav.php"; ?>
+<main>
+	<p>Richard Owusu Akomea was born on April 11, 1997, in Tarkwa, Ghana. His father, Richard Kofi Akomea, is a Reverend Pastor  and a practising Pentecostal from Amuanna Praso, Tano, and worked as a Farmer, working at the ministry of Forestry in his early career.Richard's mother, Joyce Arko Akomea, is an Akan from Akyim who worked as an English teacher and florist. His parents met after Richard held a sermon at Church of Pentecost- Koforidua, where he first interacted with Joyce, who was in attendance. He is a  citizen of Ghana. In his youth, Richard attended a Catholic day school, and formally celebrated becoming a kindergarten graduate in a religious service.</p>
+</main>
+
+<footer >CSC 174: Advanced Front-end Web Design and Development</footer>
+<?php include "inc/scripts.php";?>
+  
+</body>
+
+</html>
