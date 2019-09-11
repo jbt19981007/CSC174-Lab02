eded60a448909071f1aa6824080cea309e120d8e
rakomea <55210364+rakomea@users.noreply.github.com>
2019-09-11 16:42:56 -0400

    Add files via upload

---
+++ inc/html-top.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html lang="en">
+<head> 
+	<meta charset="utf-8">	
+	<title><?php echo $pageTitle;?></title>
+	<link rel="stylesheet" type="text/css" href="css/styles.css">
+
+</head>
