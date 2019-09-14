905e049bbbffc83d33b349e65262f86d0894393d
rakomea <55210364+rakomea@users.noreply.github.com>
2019-09-14 13:45:02 -0400

    Update homenav.php

--- inc/homenav.php
+++ inc/homenav.php
@@ -1,7 +1,7 @@
 <nav>
 		<ul>
 			<li><a href="index.php">Home</a></li>
-			<li><a href="music.php">Rafeal</a></li>
+			<li><a href="bjiang.php">Rapheal</a></li>
 			<li><a href="richard.php">Richard</a></li>
 		</ul>
 </nav>
