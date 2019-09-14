dcaa960798c82ec90f604a46e8ea9a4c213e6ad2
rakomea <55210364+rakomea@users.noreply.github.com>
2019-09-14 13:35:26 -0400

    Update nav.php

--- inc/nav.php
+++ inc/nav.php
@@ -3,6 +3,6 @@
 			<li><a href="earlylife.php">Early Life</a></li>
 			<li><a href="soccer.php">Soccer</a></li>
 			<li><a href="music.php">Music</a></li>
-			<li><a href="index.php">Home</a></li>
+			<li><a href="richard.php">Richard</a></li>
 		</ul>
 </nav>
