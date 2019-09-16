84775b32ccc7dc049bc37f69f8e59662877efab9
rakomea <55210364+rakomea@users.noreply.github.com>
2019-09-16 16:40:27 -0400

    Update soccer.php

--- soccer.php
+++ soccer.php
@@ -15,7 +15,7 @@
 	</ul>
 </aside>
 <?php include "inc/nav.php"; ?>
-<?php include "inc/homenav.php"; ?>
+<?php include "inc/homenav2.php"; ?>
 <main>
 	<h1>Soccer</h1>
 	<p>As a child, Richard played for amateur team Young Kotoko from 2011 to 2012, in Ardoggon ,Spintex Road, and later spent a couple of months with Sweps FC . In 2014, aged 17, he went on a three-day trial with Sweps FC senior team, but didn't make the final cut for the 23 man team. He subsequently moved from Accra to Gbawe, where he joined the Gbawe DC school Local pick up team. By age 10, Richard believed he had the ability to play semi-professionally, and tried to convince his mother to cease his education in order to focus entirely on soccer. While popular with other students at school, he known rather for his academic achievements than his soccer playing skills or football as it is called in Ghana and everywhere else in the world. However, one year later, he was diagnosed with a right knee contusion, a condition that could have forced him to give up playing soccer.</p> 
