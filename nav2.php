99369fd78e4d42c195dc7ee076f0f218e61463b0
MichaelHershman <55210436+MichaelHershman@users.noreply.github.com>
2019-09-16 16:21:28 -0400

    Delete nav2.php

--- nav2.php
+++
@@ -1,8 +0,0 @@
-<nav>
-		<ul>
-			<li><a href="earlylife1.php">Early Life</a></li>
-			<li><a href="swimming.php">Swimming</a></li>
-			<li><a href="stocks.php">Stocks</a></li>
-			<li><a href="michaelhershman1.php">Home</a></li>
-		</ul>
-</nav>
