f1db7f10c8b5c3ce55f752c3fbc4bd64f2dee7da
MichaelHershman <55210436+MichaelHershman@users.noreply.github.com>
2019-09-16 16:40:37 -0400

    Update stocks.php

--- stocks.php
+++ stocks.php
@@ -15,7 +15,7 @@
 	</ul>
 </aside>
 <?php include "inc/nav2.php"; ?>
-<?php include "inc/homenav.php"; ?>
+<?php include "inc/homenav3.php"; ?>
 <main>
 	<h1>Stocks</h1>
 	<p>Towards the end of Michael's highschool journey, he got into stocks. He started to use robinhood to purchase penny stocks. He watched many informational videos and started getting into reading and analyzing different kinds of data. Through the advice of many knowledgable people, Michael was able to gain very valuable information. In college, he hopes to advance his skills. He joined the university finance and investment club to learn more about the finance industry as well as more information on stock trading.</p> 
