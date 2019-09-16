52cf97d23fc3b42c3fa48833cba3671d8766acb5
rakomea <55210364+rakomea@users.noreply.github.com>
2019-09-16 16:18:21 -0400

    Delete nav2.php

--- inc/nav2.php
+++
@@ -1,8 +0,0 @@
-<nav>
-		<ul>
-			<li><a href="earlylife1.php">Early Life</a></li>
-			<li><a href="soccer.php">Soccer</a></li>
-			<li><a href="music.php">Music</a></li>
-			<li><a href="michaelhershman1.php">Home</a></li>
-		</ul>
-</nav>
