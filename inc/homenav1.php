bb318e3b400411e588561dce8258d4bdf84b6244
jbt19981007 <53458427+jbt19981007@users.noreply.github.com>
2019-09-16 16:36:30 -0400

    Create homenav1.php

---
+++ inc/homenav1.php
@@ -0,0 +1,8 @@
+<nav class="home">
+		<ul>
+			<li><a href="index.php">Home</a></li>
+			<li><a href="bjiang.php" class="is-current">Rapheal</a></li>
+			<li><a href="richard.php">Richard</a></li>
+			<li><a href="michaelhershman1.php">Michael</a></li>
+		</ul>
+</nav>
