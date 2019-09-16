5eb0713a45ed1fffe511047e2ca9d2c068edb4d0
MichaelHershman <55210436+MichaelHershman@users.noreply.github.com>
2019-09-16 16:20:37 -0400

    Add files via upload

---
+++ nav2.php
@@ -0,0 +1,8 @@
+<nav>
+		<ul>
+			<li><a href="earlylife1.php">Early Life</a></li>
+			<li><a href="swimming.php">Swimming</a></li>
+			<li><a href="stocks.php">Stocks</a></li>
+			<li><a href="michaelhershman1.php">Home</a></li>
+		</ul>
+</nav>
