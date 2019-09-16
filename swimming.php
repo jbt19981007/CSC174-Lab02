53d43b1205e4e8ceacae6848d658e28869590ade
rakomea <55210364+rakomea@users.noreply.github.com>
2019-09-16 16:14:58 -0400

    Update swimming.php

--- swimming.php
+++ swimming.php
@@ -15,6 +15,7 @@
 	</ul>
 </aside>
 <?php include "inc/nav2.php"; ?>
+<?php include "inc/homenav.php"; ?>
 <main>
 	<h1>Swimming</h1>
 	<p> From a very young age, Michael started to get into swimming. He was on a travelling competitve team. As the years went on, Michael's swimming abilities were improving a lot faster than his peers. During his senior year of highschool, he was elected to be a captain of the swim team. Although he has not swam in a competitve enviornment since highschool, he hopes to join the University swim team in the near future.</p>
