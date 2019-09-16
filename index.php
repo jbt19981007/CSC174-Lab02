30820558063136ddb271ed3c1ae91cf8756cf2e8
rakomea <55210364+rakomea@users.noreply.github.com>
2019-09-16 16:07:45 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,23 @@
 
+<?php $pageTitle="Lab 1: Initial Personal Website"; ?>
+<?php include "inc/html-top.php"; ?>
+<body class="container">
+<header >
+	<a href="index.php">
+		<h1>Richard O Akomea</h1>
+    
+        <img src="images/random.jpg" alt="home Photo">
+    </a>
+</header>
+
+<?php include "inc/homenav.php"; ?>
+<main>
+  <p> Welcome to the home of brooklyn99 </p>
+</main>
+
+<footer >CSC 174: Advanced Front-end Web Design and Development</footer>
+<?php include "inc/scripts.php";?>
+  
+</body>
+
+</html>
