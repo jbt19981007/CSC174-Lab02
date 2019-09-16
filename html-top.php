921d79de95812486820acd4428c499d235539dea
MichaelHershman <55210436+MichaelHershman@users.noreply.github.com>
2019-09-16 15:56:39 -0400

    Delete html-top.php

--- html-top.php
+++
@@ -1,8 +0,0 @@
-<!DOCTYPE html>
-<html lang="en">
-<head> 
-	<meta charset="utf-8">	
-	<title><?php echo $pageTitle;?></title>
-	<link rel="stylesheet" type="text/css" href="css/styles.css">
-
-</head>
