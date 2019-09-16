18a33e738ef2c3de615145e48fb6e2260d395e22
rakomea <55210364+rakomea@users.noreply.github.com>
2019-09-16 16:38:41 -0400

    Update nav1.php

--- inc/nav1.php
+++ inc/nav1.php
@@ -2,13 +2,13 @@
 
 		<ul>
 
-			<li><a href="bjiang.php">Raphael</a></li>
-
 			<li><a href="early_years.php">Early Years</a></li>
 
 			<li><a href="perugia.php">Perugia</a></li>
 
 			<li><a href="rome.php">Rome</a></li>
+			
+			<li><a href="bjiang.php">Raphael</a></li>
 
 		</ul>
 
